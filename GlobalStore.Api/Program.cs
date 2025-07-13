c18932c7ed34d4a3bdc88c91aa05989db496b326
Vinicius Goncalves <viniciustarga@gmail.com>
2025-07-13 14:50:35 -0300

    feat: swagger for test staging

--- GlobalStore.Api/Program.cs
+++ GlobalStore.Api/Program.cs
@@ -41,15 +41,12 @@ else
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Store Management API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Store Management API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
